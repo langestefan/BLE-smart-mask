771deeee197e4f6e10fe231a2b8c330b00a7e585
Stefan <langestefan@msn.com>
2021-05-16 13:11:57 +0200

    Add BLE advertise configuration info

---
+++ ble_smart_mask_app/configuration/ble_adv_def.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+ 
+#include <zephyr.h>
+#include <bluetooth/bluetooth.h>
+
+static const struct bt_data ad_unbonded[] = {
+      BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+      BT_DATA_BYTES(BT_DATA_UUID16_ALL,
+                    0x0f, 0x18,          /* Battery Service */
+      ),
+};
+
+static const struct bt_data ad_bonded[] = {
+      BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
+      BT_DATA_BYTES(BT_DATA_UUID16_ALL,
+                    0x0f, 0x18,   /* Battery Service */
+      ),
+};
+
+static const struct bt_data sd[] = {};
